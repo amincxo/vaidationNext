3a2880afa1d6ca5c52dc1b6e82fa8712ee1c244a
unknown <amincxo@gmail.com>
2025-05-08 01:15:06 +0330

    debug api and logout front developed button

--- pages/api/auth/signout.js
+++ pages/api/auth/signout.js
@@ -2,9 +2,9 @@ import { serialize } from "cookie";
 
 async function handler(req, res) {
     if (req.method !== "GET") return;   
-    const serialize = serialize("token", "" ,{path: "/", maxage: 0});
+    const serializeed = serialize("token", "" ,{path: "/", maxage: 0});
 
-    res.status(200).setHeader("Set-Cookie", serialized).json({status: "success", message: "Loged Out!"})
+    res.status(200).setHeader("Set-Cookie", serializeed).json({status: "success", message: "Loged Out!"})
 }
 
 export default handler
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,18 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
+
+    const sighOutHandler = async () => {
+        const res = await fetch("/api/auth/signout")
+        const data = await res.json();
+        console.log(data)
+    }
   return (
     <div className={styles.container} >
         <button><Link href="/dashboard" >Dshboard</Link> </button>
         <button><Link href="/signup" >Sign Up</Link> </button>
         <button><Link href="/signin" >Sign In</Link> </button>
+        <button onClick={sighOutHandler} >Log Out </button>
     </div>
   )
 }
