14a07ed171fc5790e7da3e713280c465db53dfbe
unknown <amincxo@gmail.com>
2025-05-08 01:00:05 +0330

    server side auth client and redirect fast

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { verifyToken } from '../utils/auth';
 
-function dashboard() {
+function Dashboard() {
   return (
-    <div>dashboard</div>
+    <div>Dashboard</div>
   )
 }
 
-export default dashboard
+export default Dashboard
+
+
+export async function getServerSideProps(context) {
+    const {token} = context.req.cookies;
+    const secretKey = process.env.SECRET_KEY;
+    const result = verifyToken(token, secretKey);
+    if (!result) {
+        return {
+            redirect: {destination: "/signin", permannent: false}
+        }
+    }
+    return {
+        props: {
+            result
+        }
+    }
+}
