e257d39c2e40828d9282c63e77f1b6a5e89f7ee3
unknown <amincxo@gmail.com>
2025-05-08 01:26:53 +0330

    sighout and log dash handled front for veiew

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
 
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { useState , useEffect } from 'react'
 
 
 export default function Home() {
+    const [isLoggedIn , setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        fetch("/api/user")
+        .then(res=> res.json())
+        .then(data => {
+            if (data.status === "success") {
+                setIsLoggedIn(true)
+            }
+        
+        })
+    },[])
 
     const sighOutHandler = async () => {
         const res = await fetch("/api/auth/signout")
         const data = await res.json();
-        console.log(data)
+        if (data.status === "success") {
+            setIsLoggedIn(false)
+        }
     }
   return (
     <div className={styles.container} >
-        <button><Link href="/dashboard" >Dshboard</Link> </button>
-        <button><Link href="/signup" >Sign Up</Link> </button>
-        <button><Link href="/signin" >Sign In</Link> </button>
-        <button onClick={sighOutHandler} >Log Out </button>
+        {isLoggedIn ?(
+            <>
+                <button><Link href="/dashboard" >Dshboard</Link> </button>
+                <button onClick={sighOutHandler} >Log Out </button>
+            </>
+        ) : null }
+        {!isLoggedIn && 
+        <>
+            <button><Link href="/signup" >Sign Up</Link> </button>
+            <button><Link href="/signin" >Sign In</Link> </button>
+        </>
+
+        }
     </div>
   )
 }
