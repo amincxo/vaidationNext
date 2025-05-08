8d193d32be4eb4798fd777b4427c7ae3d38e0257
unknown <amincxo@gmail.com>
2025-05-08 03:36:07 +0330

    api update information done

---
+++ pages/api/update-info.js
@@ -0,0 +1,48 @@
+import User from "../../models/User";
+import { verifyToken , verifyPassword } from "../../utils/auth";
+import connectDB from "../../utils/connectDB";
+
+async function handler(req,res) {
+    if(req.method !== "POST") return;
+
+    try {
+        await connectDB
+    }catch (err) {
+        console.log(err);
+        return res.status(500).json({status: "failed", message: "Error in connecting to DB"})
+    }
+
+    const {name, lastName , password} = req.body;
+    const {token} = req.cookies;
+    const secretKey = process.env.SECRET_KEY;
+
+    if (!token) {
+        return res.status(401).json({ status: "failed", message: "You are not logged in"})
+    }
+
+    const result = verifyToken(token, secretKey)
+
+    if (!result) {
+        return res.status(401).json({ status: "failed", message: "You are unauthirized"})
+    }
+    const user = await User.findOne({email: result.email})
+    if (!user) {
+        return res.status(404).json({
+            status: "failed",
+            message: "User doesn't exist!"
+        })
+    }
+    const isValid = await verifyPassword(password, user.password)
+    if (!isValid) {
+        return res.status(423).json({
+            status: "failed",
+            message: "Username or password is in correct!",
+        });
+    }
+    user.name = name;
+    user.lastName = lastName;
+    user.save();
+}
+
+
+export default handler
